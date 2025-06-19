841f86ca9c65bc7e3d8dab9016ba7ab8d8aeb9b9
zuldandewa <zuldanprstya@gmail.com>
2025-06-19 12:49:46 +0700

    penambahan  file config untuk slam_toolbox

--- launch/joystick.launch.py
+++ launch/joystick.launch.py
@@ -7,27 +7,25 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    # use_sim_time = LaunchConfiguration('use_sim_time')
+#     use_sim_time = LaunchConfiguration('use_sim_time')
 
     joy_params = os.path.join(get_package_share_directory('jelly_fish'),'config','joystick.yaml')
 
     joy_node = Node(
             package='joy',
             executable='joy_node',
-            parameters=[joy_params],
+            parameters=[joy_params],  #, {'use_sim_time': use_sim_time}
          )
-    # , {'use_sim_time': use_sim_time}
     
     teleop_node = Node(
             package='teleop_twist_joy',
             executable='teleop_node',
             name='teleop_node',
-            parameters=[joy_params],
-            remappings=[('/cmd_vel','/diff_cont/cmd_vel_unstamped')]    
-         )
-    # , {'use_sim_time': use_sim_time}],
-            # remappings=[('/cmd_vel','/cmd_vel_joy')
+            parameters=[joy_params],            #, {'use_sim_time': use_sim_time}
+            remappings=[('/cmd_vel','/diff_cont/cmd_vel_unstamped')]
     
+         )
+ 
     return LaunchDescription([
         # DeclareLaunchArgument(
         #     'use_sim_time',
